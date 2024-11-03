f03d723cddc9b077d67a92e138c7a67ffb9211ed
MosesJCoker <joshuamcoker@gmail.com>
2024-11-03 18:36:25 +0000

    folder structure added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Generative AI Project',
+    version= '0.0.0',
+    author= 'Moses J.Coker',
+    author_email= 'joshuamcoker@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
